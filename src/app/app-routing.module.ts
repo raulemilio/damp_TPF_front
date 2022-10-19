27f34de9faad1684a35415a8fe756b4297d8cc96
Raul Emilio Romero <rraulemilioromero@gmail.com>
2022-10-19 15:26:45 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+// estas rutas permiten el direccionamiento entre las vistas de los componentes
 const routes: Routes = [
   {
     path: 'home',
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'dispositivo/:id',
+    loadChildren: () => import('./dispositivo/dispositivo.module').then( m => m.DispositivoPageModule)
+  },
+  {
+    path: 'medicion/:id',
+    loadChildren: () => import('./medicion/medicion.module').then( m => m.MedicionPageModule)
+  },
+  {
+    path: 'log/:id',
+    loadChildren: () => import('./log/log.module').then( m => m.LogPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DispositivoService } from '../services/dispositivo.service';
+import { Dispositivo } from '../model/Dispositivo';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listadoDispositivo:Dispositivo[];
 
+  constructor(public dispositivoServ:DispositivoService) {
+    // Se solicita a la Backend el listado de dispositivos disponibles en la BD
+    this.dispService();
+  }
+  // función para solicitar al Backend el listado de dispositivos
+  async dispService(){
+    let listado= await this.dispositivoServ.getListadoDispositivos();
+    this.listadoDispositivo=listado;
+  }
 }
